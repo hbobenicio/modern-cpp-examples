37f9d1e939ab75dfb295f7867d071c3ad32b84d1
Hugo Benício <hbobenicio@gmail.com>
2016-12-05 02:40:16 -0300

    Finishing chrono example

--- chrono/chrono.cpp
+++ chrono/chrono.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
 #include <chrono>
+#include <ctime>
 
 static void doSomeWork()
 {
   std::cout << "Doing some work for measuring." << '\n';
+  for (int i = 0; i < 10000; i++) {
+    for (int j = 0; j < 100000; j++) {
+      i--;
+      i++;
+    }
+  }
 }
 
 static void measuringTime()
@@ -16,12 +23,21 @@ static void measuringTime()
   doSomeWork();
   auto end = steady_clock::now();
 
-  auto us = duration_cast<microseconds>(end - start).count();
-  cout << "doSomeWork() run for " << us << " microseconds\n";
+  auto us = duration_cast<seconds>(end - start).count();
+  cout << "doSomeWork() run for " << us << " seconds\n";
 }
 
 int main()
 {
+  using std::cout;
+
   measuringTime();
+
+  // You can use chrono literals to create duration objects
+  using namespace std::literals::chrono_literals;
+  auto day = 24h; // deduced type = std::chrono::hours
+  auto halfHour = 0.5h;
+  std::cout << "one day is " << day.count() << " hours\n"
+            << "half an hour is " << halfHour.count() << " hours\n"; 
 }
 
