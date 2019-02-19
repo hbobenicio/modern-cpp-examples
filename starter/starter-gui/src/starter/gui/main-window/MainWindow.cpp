e7e1242f106cc62954c243d582ea6d390b2b16a6
Hugo Benicio Miranda de Oliveira <hugo.oliveira@serpro.gov.br>
2019-02-19 17:11:09 -0300

    Getting all widget refs from glade ui

--- starter/starter-gui/src/starter/gui/main-window/MainWindow.cpp
+++ starter/starter-gui/src/starter/gui/main-window/MainWindow.cpp
@@ -9,11 +9,45 @@ namespace starter { namespace gui { namespace mainwindow {
     {
         std::cout << "[starter::gui::mainwindow::MainWindow::MainWindow] ";
         std::cout << "inicializando widget a partir do builder glade...\n";
+
+        this->getWidgets(builder);
+        this->addApps();
     }
 
     MainWindow::~MainWindow() {
         std::cout << "[starter::gui::mainwindow::MainWindow::~MainWindow] ";
         std::cout << "destruindo MainWindow\n";
+    }
+
+    void MainWindow::getWidgets(const Glib::RefPtr<Gtk::Builder>& builder) {
+        builder->get_widget("titleLabel", this->titleLabel);
+        if (!this->titleLabel) {
+            std::cerr << "[starter::gui::mainwindow::MainWindow::getWidgets]";
+            std::cerr << "não foi possível carregar widget 'titleLabel'";
+        }
+
+        builder->get_widget("mainBox", this->mainBox);
+        if (!this->mainBox) {
+            std::cerr << "[starter::gui::mainwindow::MainWindow::getWidgets]";
+            std::cerr << "não foi possível carregar widget 'mainBox'";
+        }
+
+        builder->get_widget("appListBox", this->appListBox);
+        if (!this->appListBox) {
+            std::cerr << "[starter::gui::mainwindow::MainWindow::getWidgets]";
+            std::cerr << "não foi possível carregar widget 'appListBox'";
+        }
+    }
+
+    void MainWindow::addApps() {
+        std::vector<std::string> apps;
+        apps.emplace_back("Firefox Quantum");
+        apps.emplace_back("Firefox Serpro");
+        apps.emplace_back("Gnome Terminal");
+
+        for (const auto app: apps) {
+            std::cout << "TODO: adicionar app " << app << " na appListBox...\n";
+        }
     }
 
 }}}
--- starter/starter-gui/src/starter/gui/main-window/MainWindow.h
+++ starter/starter-gui/src/starter/gui/main-window/MainWindow.h
@@ -1,15 +1,28 @@
 #ifndef STARTER_GUI_MAIN_WINDOW_H
 #define STARTER_GUI_MAIN_WINDOW_H
 
-#include <gtkmm.h>
+#include <gtkmm/window.h>
+#include <gtkmm/builder.h>
+#include <gtkmm/label.h>
+#include <gtkmm/box.h>
+#include <gtkmm/listbox.h>
 
 namespace starter { namespace gui { namespace mainwindow {
 
     class MainWindow: public Gtk::Window
     {
+    private:
+        Gtk::Label* titleLabel = nullptr;
+        Gtk::Box* mainBox = nullptr;
+        Gtk::ListBox* appListBox = nullptr;
+
     public:
-        MainWindow(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade);
+        MainWindow(Gtk::Window::BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade);
         virtual ~MainWindow();
+
+    private:
+        void getWidgets(const Glib::RefPtr<Gtk::Builder>& builder);
+        void addApps();
     };
 
 }}}
